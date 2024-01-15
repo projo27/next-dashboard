4852f295e019a2a4094481b3e4a8769f36859f52
Nafi <projo27@gmail.com>
2024-01-15 08:46:42 +0700

    chapter/10

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -13,7 +13,7 @@ import {
 import { formatCurrency } from './utils';
 
 export async function fetchRevenue() {
-  noStore();
+
   // Add noStore() here to prevent the response from being cached.
   // This is equivalent to in fetch(..., {cache: 'no-store'}).
   noStore();
@@ -39,12 +39,16 @@ export async function fetchRevenue() {
 export async function fetchLatestInvoices() {
   noStore();
   try {
+    await new Promise((resolve) => setTimeout(resolve, 3000));
+
     const data = await sql<LatestInvoiceRaw>`
       SELECT invoices.amount, customers.name, customers.image_url, customers.email, invoices.id
       FROM invoices
       JOIN customers ON invoices.customer_id = customers.id
       ORDER BY invoices.date DESC
       LIMIT 5`;
+
+    console.log('Data fetch latest invoices completed after 3 seconds.');
 
     const latestInvoices = data.rows.map((invoice) => ({
       ...invoice,
